b16d0473e30a2431cb5defd34231c1238a8f443c
lpruissen1 <leepruissen@gmail.com>
2021-02-06 17:49:41 -0500

    Add tests for the post request

--- API/UserCustomIndicesTests/Controllers/CustomIndexControllerPostTest.cs
+++ API/UserCustomIndicesTests/Controllers/CustomIndexControllerPostTest.cs
@@ -1,5 +1,6 @@
 ﻿using Database.Model.User.CustomIndices;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -15,17 +16,42 @@ namespace UserCustomIndicesTests.Controllers
         private ICustomIndexService customIndexService;
         private CustomIndexController sut;
 
+        private Mock<ICustomIndexValidator> CustomIndexValidatorMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            CustomIndexValidatorMock = new Mock<ICustomIndexValidator>();
+        }
+
         [Test]
         public void TestPost_InvalidCustomIndex()
         {
+            var index = new CustomIndex();
+            CustomIndexValidatorMock.Setup(x => x.Validate(index)).Returns(false);
+
+            SetupController(CustomIndexValidatorMock.Object);
+
+            var response = sut.Create(index).Result;
+
+            Assert.IsInstanceOf<BadRequestResult>(response);
         }
 
         [Test]
         public void TestPost_ValidCustomIndex()
         {
+            var index = new CustomIndex();
+            CustomIndexValidatorMock.Setup(x => x.Validate(index)).Returns(false);
+
+            SetupController(CustomIndexValidatorMock.Object);
+
+            var response = sut.Create(index);
+
+            Assert.IsInstanceOf<CreatedAtRouteResult>(response.Result);
+            Assert.AreEqual(index, response.Value);
         }
 
-        private void SetupCustomIndexCollection(ICustomIndexValidator validator)
+        private void SetupController(ICustomIndexValidator validator)
         {
             customIndexService = new CustomIndexServiceFake(new List<CustomIndex>(), new Dictionary<Guid, List<string>>());
             sut = new CustomIndexController(customIndexService, validator);
