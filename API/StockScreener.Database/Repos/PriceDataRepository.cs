4f46b313a325aafe74fc6bcda46f78dec08bde47
lee pruissen <leepruissen@gmail.com>
2021-04-01 22:05:43 -0400

    Merge conflict:

--- API/StockScreener.Database/Repos/PriceDataRepository.cs
+++ API/StockScreener.Database/Repos/PriceDataRepository.cs
@@ -46,10 +46,11 @@ namespace StockScreener.Database.Repos
 			var prices = mongoContext.GetCollection<TPriceEntry>(typeof(TPriceEntry).Name).Find(filter).FirstOrDefault();
 			return prices?.Candle.Max(x => x.timestamp) ?? 0;
 		}
-
-        public double GetPriceData<TPriceEntry>(string ticker, TimeSpan timeSpan) where TPriceEntry : PriceData
-        {
-            throw new NotImplementedException();
+		public List<Candle> GetPriceData<TPriceEntry>(string ticker) where TPriceEntry : PriceData
+		{
+			var filter = Builders<TPriceEntry>.Filter.Eq(e => e.Ticker, ticker);
+			var prices = mongoContext.GetCollection<TPriceEntry>(typeof(TPriceEntry).Name).Find(filter).FirstOrDefault();
+			return prices.Candle;
 		}
 
 		private void UpdatePriceData<TPriceType>(TPriceType entry) where TPriceType : PriceData
