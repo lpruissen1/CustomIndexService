940ed60b2755ce18074354a9c1e5ec355329051d
Alexander Davis <alexander96davis@gmail.com>
2021-04-07 17:08:24 -0400

    blah

--- API/StockScreener.Service.IntegrationTests/AnnualizedDividendGrowthScreeningTests.cs
+++ API/StockScreener.Service.IntegrationTests/AnnualizedDividendGrowthScreeningTests.cs
@@ -69,5 +69,89 @@ namespace StockScreener.Service.IntegrationTests
 
 			Assert.AreEqual(ticker1, result[0].Ticker);
 		}
+
+		[Test]
+		public void ScreenByStockIndex_DividendGrowthAnnualized_Yearly()
+		{
+			var stockIndex1 = "Lee's Index";
+
+			var ticker1 = "LEE";
+			var ticker2 = "PEE";
+
+			InsertData(StockIndexCreator.GetStockIndex(stockIndex1).AddTicker(ticker1).AddTicker(ticker2));
+
+			InsertData(StockFinancialsCreator.GetStockFinancials(ticker1)
+				.AddDividendsPerShare(0.18d, 1554004800)
+				.AddDividendsPerShare(0.25d, 1585627200));
+
+			InsertData(StockFinancialsCreator.GetStockFinancials(ticker2)
+				.AddDividendsPerShare(0.75d, 1554004800)
+				.AddDividendsPerShare(0.79d, 1585627200));
+
+			AddMarketToCustomIndex(stockIndex1);
+			AddAnnualizedDividendGrowthToCustomIndex(40, 25, 4);
+
+			var result = sut.Screen(customIndex);
+
+			Assert.AreEqual(1, result.Count);
+
+			Assert.AreEqual(ticker1, result[0].Ticker);
+		}
+
+		[Test]
+		public void ScreenByStockIndex_DividendGrowthAnnualized_ThreeYear()
+		{
+			var stockIndex1 = "Lee's Index";
+
+			var ticker1 = "LEE";
+			var ticker2 = "PEE";
+
+			InsertData(StockIndexCreator.GetStockIndex(stockIndex1).AddTicker(ticker1).AddTicker(ticker2));
+
+			InsertData(StockFinancialsCreator.GetStockFinancials(ticker1)
+				.AddDividendsPerShare(0.15d, 1490954000)
+				.AddDividendsPerShare(0.25d, 1585627200));
+
+			InsertData(StockFinancialsCreator.GetStockFinancials(ticker2)
+				.AddDividendsPerShare(0.75d, 1490954000)
+				.AddDividendsPerShare(0.79d, 1585627200));
+
+			AddMarketToCustomIndex(stockIndex1);
+			AddAnnualizedDividendGrowthToCustomIndex(20, 10, 12);
+
+			var result = sut.Screen(customIndex);
+
+			Assert.AreEqual(1, result.Count);
+
+			Assert.AreEqual(ticker1, result[0].Ticker);
+		}
+
+		[Test]
+		public void ScreenByStockIndex_DividendGrowthAnnualized_FiveYear()
+		{
+			var stockIndex1 = "Lee's Index";
+
+			var ticker1 = "LEE";
+			var ticker2 = "PEE";
+
+			InsertData(StockIndexCreator.GetStockIndex(stockIndex1).AddTicker(ticker1).AddTicker(ticker2));
+
+			InsertData(StockFinancialsCreator.GetStockFinancials(ticker1)
+				.AddDividendsPerShare(0.15d, 1427839200)
+				.AddDividendsPerShare(0.25d, 1585627200));
+
+			InsertData(StockFinancialsCreator.GetStockFinancials(ticker2)
+				.AddDividendsPerShare(0.75d, 1427839200)
+				.AddDividendsPerShare(0.79d, 1585627200));
+
+			AddMarketToCustomIndex(stockIndex1);
+			AddAnnualizedDividendGrowthToCustomIndex(11, 10, 20);
+
+			var result = sut.Screen(customIndex);
+
+			Assert.AreEqual(1, result.Count);
+
+			Assert.AreEqual(ticker1, result[0].Ticker);
+		}
 	}
 }
