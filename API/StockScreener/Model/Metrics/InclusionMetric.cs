7631deb5b5c20a9af9bb32094b4fa47753784e1d
lpruissen1 <46303264+lpruissen1@users.noreply.github.com>
2021-06-12 14:53:53 -0400

    Add inclusion and exclusion metric

---
+++ API/StockScreener/Model/Metrics/InclusionMetric.cs
@@ -0,0 +1,39 @@
+﻿using StockScreener.Calculators;
+using StockScreener.Core;
+using StockScreener.Model.BaseSecurity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StockScreener.Model.Metrics
+{
+	public class InclusionMetric : IMetric
+    {
+        public InclusionMetric(List<string> includedTickers)
+        {
+			this.includedTickers = includedTickers;
+        }
+
+        private List<string> includedTickers { get; init; }
+
+        public void Apply(ref SecuritiesList<DerivedSecurity> securitiesList)
+        {
+			var tempSecurityList = securitiesList;
+			var newSecurities = includedTickers.Where(includedTicker => !tempSecurityList.Any(security =>  includedTicker == security.Ticker));
+
+			foreach (var ticker in newSecurities)
+			{
+				securitiesList.Add(new DerivedSecurity { Ticker = ticker });
+			}
+        }
+
+		public IEnumerable<BaseDatapoint> GetBaseDatapoints()
+		{
+			return Enumerable.Empty<BaseDatapoint>();
+		}
+
+		public IEnumerable<DerivedDatapointConstructionData> GetDerivedDatapoints()
+		{
+			return Enumerable.Empty<DerivedDatapointConstructionData>();
+		}
+	}
+}
