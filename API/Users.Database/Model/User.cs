c9d0accd7395293c8e476b2dc81781e51e903f1e
Alexander Davis <alexander96davis@gmail.com>
2021-07-24 21:49:46 -0400

    Begin expanding user model, need to make Upgrade method

--- API/Users.Database/Model/User.cs
+++ API/Users.Database/Model/User.cs
@@ -6,9 +6,14 @@ namespace Users.Database.Model
 	public class User : DbEntity
 	{
 		public string UserId { get; set; }
+		public string AccountType { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string UserName { get; set; }
 		public string EmailAddress { get; set; }
+		public string PhoneNumber { get; set; }
+		public string StreetAddress { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
 	}
 }
