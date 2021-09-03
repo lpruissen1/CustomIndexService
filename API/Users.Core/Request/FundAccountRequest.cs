2f9e7600113d61ceaecebe602b5bff1a4eba26ec
Alexander Davis <alexander96davis@gmail.com>
2021-09-02 22:18:08 -0400

    l8 nite

--- API/Users.Core/Request/FundAccountRequest.cs
+++ API/Users.Core/Request/FundAccountRequest.cs
@@ -4,7 +4,6 @@ namespace Users.Core.Request
 {
 	public class FundAccountRequest
 	{
-		public string AlpacaAccountId { get; set; }
 		public string TransferType { get; set; }
 		public string RelationshipId { get; set; }
 		public string Amount { get; set; }
